8a32f8db2c9f9820511ebae17fdd4da09d1663ec
Paavo Palomäki <paavo.palomaki@gmail.com>
2025-03-31 23:53:40 +0300

    Battery simulation

--- software/air-quality-sensor-nrf52/src/battery.c
+++ software/air-quality-sensor-nrf52/src/battery.c
@@ -1,8 +1,15 @@
 #include <battery.h>
 #include <variables.h>
 
+float battery_lvl = 100;
+
 int read_battery_level(void)
 {
-	set_battery_level(100.0);
+	set_battery_level(battery_lvl);
+	battery_lvl = battery_lvl - 1;
+	if (battery_lvl <= 0)
+	{
+		battery_lvl = 100;
+	}
 	return 0;
 }
