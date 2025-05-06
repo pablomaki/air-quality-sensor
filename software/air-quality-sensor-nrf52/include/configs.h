78402b97a43ebf33cd6b4f4f8ceeb3e543da94ce
Paavo Palomäki <paavo.palomaki@gmail.com>
2025-05-06 22:56:59 +0300

    Add comments to config variables

--- software/air-quality-sensor-nrf52/include/configs.h
+++ software/air-quality-sensor-nrf52/include/configs.h
@@ -20,10 +20,10 @@
 // #define ENABLE_STATE_LED     // Enable or disable LED for showing the current status
 
 // Sensors
-#define ENABLE_SHT4X
-#define ENABLE_SGP40
-#define ENABLE_SCD4X
-#define ENABLE_BMP390
+#define ENABLE_SHT4X  // Enable or disable SHT4X sensor
+#define ENABLE_SGP40  // Enable or disable SGP40 sensor
+#define ENABLE_SCD4X  // Enable or disable SCD4X sensor
+#define ENABLE_BMP390 // Enable or disable BMP390 sensor
 
 // Environment
 #define ALTITUDE 10          // meters
@@ -33,35 +33,35 @@
 // #define ENABLE_EPD //Enable or disable the screen
 
 // Battery monitor
-#define ENABLE_BATTERY_MONITOR
-#define BATTERY_TYPE_SAMSUNG_Q30 // Used battery type currently supported choices: BATTERY_TYPE_SAMSUNG_Q30
-#define BM_ADC_ID 0
-#define USE_FAST_CHARGING 1 // 0 for slow (50 mA) charging, 1 for fast (100 mA) charging
-#define ADC_SAMPLES_TOTAL 10
-#define ADC_SAMPLE_INTERVAL_US 500
-#define BM_ADC_RESOLUTION 12
-#define VOLTAGE_DIVIDER_R1 1031 // 1Mohm
-#define VOLTAGE_DIVIDER_R2 510  // 510kohm
+#define ENABLE_BATTERY_MONITOR     // Enable or disable battery monitor
+#define BATTERY_TYPE_SAMSUNG_Q30   // Used battery type currently supported choices: BATTERY_TYPE_SAMSUNG_Q30
+#define BM_ADC_ID 0                // ADC channel for battery monitor
+#define USE_FAST_CHARGING 1        // 0 for slow (50 mA) charging, 1 for fast (100 mA) charging
+#define ADC_SAMPLES_TOTAL 10       // Number of samples to take for averaging
+#define ADC_SAMPLE_INTERVAL_US 500 // Microseconds
+#define BM_ADC_RESOLUTION 12       // ADC resolution in bits
+#define VOLTAGE_DIVIDER_R1 1031    // 1Mohm
+#define VOLTAGE_DIVIDER_R2 510     // 510kohm
 
 // Value scaling and min max for warning
-#define TEMPERATURE_SCALE 100
-#define TEMPERATURE_MIN -100
-#define TEMPERATURE_MAX 100
+#define TEMPERATURE_SCALE 100 // 100 for 2 decimal places
+#define TEMPERATURE_MIN -100  // Celsius
+#define TEMPERATURE_MAX 100   // Celsius
 
-#define HUMIDITY_SCALE 100
-#define HUMIDITY_MIN 0
-#define HUMIDITY_MAX 100
+#define HUMIDITY_SCALE 100 // 100 for 2 decimal places
+#define HUMIDITY_MIN 0     // Percentage
+#define HUMIDITY_MAX 100   // Percentage
 
-#define PRESSURE_SCALE 0.1
-#define PRESSURE_MIN 0
-#define PRESSURE_MAX 200000
+#define PRESSURE_SCALE 0.1f // 0.1 for -1 decimal place
+#define PRESSURE_MIN 0      // Pascals
+#define PRESSURE_MAX 200000 // Pascals
 
-#define CO2_CONCENTRATION_SCALE 10
-#define CO2_CONCENTRATION_MIN 0
-#define CO2_CONCENTRATION_MAX 10000
+#define CO2_CONCENTRATION_SCALE 10  // 10 for 1 decimal place
+#define CO2_CONCENTRATION_MIN 0     // ppm
+#define CO2_CONCENTRATION_MAX 10000 // ppm
 
-#define VOC_INDEX_SCALE 10
-#define VOC_INDEX_MIN 0
-#define VOC_INDEX_MAX 500
+#define VOC_INDEX_SCALE 10 // 10 for 1 decimal place
+#define VOC_INDEX_MIN 0    // index value (0-500)
+#define VOC_INDEX_MAX 500  // index value (0-500)
 
 #endif
