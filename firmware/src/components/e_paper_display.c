69a35d30572e01d227c12fc5433197c330d4bf08
Paavo Palomäki <paavo.palomaki@gmail.com>
2025-08-29 23:57:23 +0300

    Turn the screen 90 degrees CW, change content to simpler format

--- firmware/src/components/e_paper_display.c
+++ firmware/src/components/e_paper_display.c
@@ -1,7 +1,6 @@
 #include <components/e_paper_display.h>
 #include <utils/variable_buffer.h>
 #include <utils/air_quality_mapper.h>
-#include <utils/lv_font_montserrat_64.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
@@ -21,19 +20,15 @@ static const struct device *epd_dev;
 
 static lv_obj_t *notif_label;
 static lv_obj_t *temp_tag_label;
-static lv_obj_t *temp_ms_label;
-static lv_obj_t *temp_ls_label;
-static lv_obj_t *temp_unit_label;
+static lv_obj_t *temp_val_label;
 static lv_obj_t *hum_tag_label;
 static lv_obj_t *hum_val_label;
-static lv_obj_t *hum_unit1_label;
-static lv_obj_t *hum_unit2_label;
-static lv_obj_t *co2_tag1_label;
-static lv_obj_t *co2_tag2_label;
+static lv_obj_t *co2_tag_label;
 static lv_obj_t *co2_val_label;
-static lv_obj_t *voc_val_label;
 static lv_obj_t *voc_tag_label;
-static lv_obj_t *battery_percentage_label;
+static lv_obj_t *voc_val_label;
+static lv_obj_t *batt_tag_label;
+static lv_obj_t *batt_val_label;
 
 static char label_buffer[32];
 
@@ -46,70 +41,61 @@ int init_e_paper_display(void)
         return -ENXIO;
     }
 
+    // Configure LVGL for rotated display
+    lv_disp_t *disp = lv_disp_get_default();
+    if (disp)
+    {
+        lv_disp_set_rotation(disp, LV_DISP_ROT_90);
+    }
+
     // Setup notification label
     notif_label = lv_label_create(lv_scr_act());
-    lv_obj_set_style_text_font(notif_label, &lv_font_montserrat_64, 0);
+    lv_obj_set_style_text_font(notif_label, &lv_font_montserrat_24, 0);
     lv_obj_align(notif_label, LV_ALIGN_CENTER, 0, 0);
 
     // Set up temperature labels
     temp_tag_label = lv_label_create(lv_scr_act());
-    temp_ms_label = lv_label_create(lv_scr_act());
-    temp_ls_label = lv_label_create(lv_scr_act());
-    temp_unit_label = lv_label_create(lv_scr_act());
+    temp_val_label = lv_label_create(lv_scr_act());
     lv_obj_set_style_text_font(temp_tag_label, &lv_font_montserrat_16, 0);
-    lv_obj_set_style_text_font(temp_ms_label, &lv_font_montserrat_64, 0);
-    lv_obj_set_style_text_font(temp_ls_label, &lv_font_montserrat_32, 0);
-    lv_obj_set_style_text_font(temp_unit_label, &lv_font_montserrat_24, 0);
-    lv_obj_align(temp_tag_label, LV_ALIGN_BOTTOM_RIGHT, -137, -115);
-    lv_obj_align(temp_ms_label, LV_ALIGN_BOTTOM_RIGHT, -165, -59);
-    lv_obj_align(temp_ls_label, LV_ALIGN_BOTTOM_LEFT, 85, -87);
-    lv_obj_align(temp_unit_label, LV_ALIGN_BOTTOM_LEFT, 85, -66);
-    snprintf(label_buffer, sizeof(label_buffer), "Temperature");
-    lv_label_set_text(temp_tag_label, label_buffer);
-    snprintf(label_buffer, sizeof(label_buffer), "\xB0"
-                                                 "C");
-    lv_label_set_text(temp_unit_label, label_buffer);
+    lv_obj_set_style_text_font(temp_val_label, &lv_font_montserrat_32, 0);
+    lv_obj_align(temp_tag_label, LV_ALIGN_TOP_LEFT, 10, 0);
+    lv_obj_align(temp_val_label, LV_ALIGN_TOP_RIGHT, -12, 18);
 
     // Set up humidity labels
     hum_tag_label = lv_label_create(lv_scr_act());
     hum_val_label = lv_label_create(lv_scr_act());
-    hum_unit1_label = lv_label_create(lv_scr_act());
-    hum_unit2_label = lv_label_create(lv_scr_act());
     lv_obj_set_style_text_font(hum_tag_label, &lv_font_montserrat_16, 0);
-    lv_obj_set_style_text_font(hum_val_label, &lv_font_montserrat_64, 0);
-    lv_obj_set_style_text_font(hum_unit1_label, &lv_font_montserrat_32, 0);
-    lv_obj_set_style_text_font(hum_unit2_label, &lv_font_montserrat_20, 0);
-    lv_obj_align(hum_tag_label, LV_ALIGN_BOTTOM_RIGHT, -137, -54);
-    lv_obj_align(hum_val_label, LV_ALIGN_BOTTOM_RIGHT, -165, 3);
-    lv_obj_align(hum_unit1_label, LV_ALIGN_BOTTOM_LEFT, 85, -23);
-    lv_obj_align(hum_unit2_label, LV_ALIGN_BOTTOM_LEFT, 82, -6);
+    lv_obj_set_style_text_font(hum_val_label, &lv_font_montserrat_32, 0);
+    lv_obj_align(hum_tag_label, LV_ALIGN_TOP_LEFT, 10, 52);
+    lv_obj_align(hum_val_label, LV_ALIGN_TOP_RIGHT, -12, 70);
 
     // Set up CO2 labels
-    co2_tag1_label = lv_label_create(lv_scr_act());
-    co2_tag2_label = lv_label_create(lv_scr_act());
+    co2_tag_label = lv_label_create(lv_scr_act());
     co2_val_label = lv_label_create(lv_scr_act());
-    lv_obj_set_style_text_font(co2_tag1_label, &lv_font_montserrat_16, 0);
-    lv_obj_set_style_text_font(co2_tag2_label, &lv_font_montserrat_10, 0);
-    lv_obj_set_style_text_font(co2_val_label, &lv_font_montserrat_48, 0);
-    lv_obj_align(co2_tag1_label, LV_ALIGN_BOTTOM_RIGHT, 0, -115);
-    lv_obj_align(co2_tag2_label, LV_ALIGN_BOTTOM_RIGHT, -50, -114);
-    lv_obj_align(co2_val_label, LV_ALIGN_BOTTOM_RIGHT, 0, -68);
+    lv_obj_set_style_text_font(co2_tag_label, &lv_font_montserrat_16, 0);
+    lv_obj_set_style_text_font(co2_val_label, &lv_font_montserrat_32, 0);
+    lv_obj_align(co2_tag_label, LV_ALIGN_TOP_LEFT, 10, 104);
+    lv_obj_align(co2_val_label, LV_ALIGN_TOP_RIGHT, -12, 122);
 
     // Set up VOC labels
     voc_val_label = lv_label_create(lv_scr_act());
     voc_tag_label = lv_label_create(lv_scr_act());
     lv_obj_set_style_text_font(voc_tag_label, &lv_font_montserrat_16, 0);
-    lv_obj_set_style_text_font(voc_val_label, &lv_font_montserrat_28, 0);
-    lv_obj_align(voc_tag_label, LV_ALIGN_BOTTOM_RIGHT, 0, -55);
-    lv_obj_align(voc_val_label, LV_ALIGN_BOTTOM_RIGHT, 0, -28);
+    lv_obj_set_style_text_font(voc_val_label, &lv_font_montserrat_24, 0);
+    lv_obj_align(voc_tag_label, LV_ALIGN_TOP_LEFT, 10, 156);
+    lv_obj_align(voc_val_label, LV_ALIGN_TOP_RIGHT, -12, 173);
 
     // Set up battery percentage label
-    battery_percentage_label = lv_label_create(lv_scr_act());
-    lv_obj_set_style_text_font(battery_percentage_label, &lv_font_montserrat_20, 0);
-    lv_obj_align(battery_percentage_label, LV_ALIGN_BOTTOM_RIGHT, -3, -6);
+    batt_tag_label = lv_label_create(lv_scr_act());
+    batt_val_label = lv_label_create(lv_scr_act());
+    lv_obj_set_style_text_font(batt_tag_label, &lv_font_montserrat_16, 0);
+    lv_obj_set_style_text_font(batt_val_label, &lv_font_montserrat_32, 0);
+    lv_obj_align(batt_tag_label, LV_ALIGN_TOP_LEFT, 10, 198);
+    lv_obj_align(batt_val_label, LV_ALIGN_TOP_RIGHT, -12, 215);
 
-    // Turn off display blanking for ???
+    // Turn off display blanking to activate partial updates
     display_blanking_off(epd_dev);
+
     return 0;
 }
 
@@ -117,19 +103,15 @@ int display_notification(const char *message)
 {
     lv_label_set_text(notif_label, "");
     lv_label_set_text(temp_tag_label, "");
-    lv_label_set_text(temp_ms_label, "");
-    lv_label_set_text(temp_ls_label, "");
-    lv_label_set_text(temp_unit_label, "");
+    lv_label_set_text(temp_val_label, "");
     lv_label_set_text(hum_tag_label, "");
     lv_label_set_text(hum_val_label, "");
-    lv_label_set_text(hum_unit1_label, "");
-    lv_label_set_text(hum_unit2_label, "");
-    lv_label_set_text(co2_tag1_label, "");
-    lv_label_set_text(co2_tag2_label, "");
+    lv_label_set_text(co2_tag_label, "");
     lv_label_set_text(co2_val_label, "");
     lv_label_set_text(voc_val_label, "");
     lv_label_set_text(voc_tag_label, "");
-    lv_label_set_text(battery_percentage_label, "");
+    lv_label_set_text(batt_tag_label, "");
+    lv_label_set_text(batt_val_label, "");
     lv_label_set_text(notif_label, message);
     lv_task_handler();
     return 0;
@@ -137,49 +119,44 @@ int display_notification(const char *message)
 
 int update_e_paper_display(void)
 {
+    static int refresh_count = 0;
+
     lv_label_set_text(notif_label, "");
     snprintf(label_buffer, sizeof(label_buffer), "Temperature");
     lv_label_set_text(temp_tag_label, label_buffer);
-    snprintf(label_buffer, sizeof(label_buffer), "\xB0"
-                                                 "C");
-    lv_label_set_text(temp_unit_label, label_buffer);
     snprintf(label_buffer, sizeof(label_buffer), "Humidity");
     lv_label_set_text(hum_tag_label, label_buffer);
-    snprintf(label_buffer, sizeof(label_buffer), "%%");
-    lv_label_set_text(hum_unit1_label, label_buffer);
-    snprintf(label_buffer, sizeof(label_buffer), "RH");
-    lv_label_set_text(hum_unit2_label, label_buffer);
-    snprintf(label_buffer, sizeof(label_buffer), "CO  (ppm)");
-    lv_label_set_text(co2_tag1_label, label_buffer);
-    snprintf(label_buffer, sizeof(label_buffer), "2");
-    lv_label_set_text(co2_tag2_label, label_buffer);
+    snprintf(label_buffer, sizeof(label_buffer), "CO2 (ppm)");
+    lv_label_set_text(co2_tag_label, label_buffer);
     snprintf(label_buffer, sizeof(label_buffer), "Air quality");
     lv_label_set_text(voc_tag_label, label_buffer);
+    snprintf(label_buffer, sizeof(label_buffer), "Battery");
+    lv_label_set_text(batt_tag_label, label_buffer);
 
     // Set temperature
     float temp = get_latest(TEMPERATURE);
     if (temp == -1)
     {
-        lv_label_set_text(temp_ms_label, "n/a");
-        lv_label_set_text(temp_ls_label, ".-");
+        lv_label_set_text(temp_val_label, "n/a");
     }
     else
     {
-        snprintf(label_buffer, sizeof(label_buffer), "%d", (int)temp);
-        lv_label_set_text(temp_ms_label, label_buffer);
-        snprintf(label_buffer, sizeof(label_buffer), ".%01d", (int)((temp - (int)temp) * 10));
-        lv_label_set_text(temp_ls_label, label_buffer);
+        snprintf(label_buffer, sizeof(label_buffer), "%d.%01d"
+                                                     "\xB0"
+                                                     "C",
+                 (int)temp, (int)((temp - (int)temp) * 10));
+        lv_label_set_text(temp_val_label, label_buffer);
     }
 
     // Set humidity
     float hum = get_latest(HUMIDITY);
-    if (temp == -1)
+    if (hum == -1)
     {
         lv_label_set_text(hum_val_label, "n/a");
     }
     else
     {
-        snprintf(label_buffer, sizeof(label_buffer), "%d", (int)hum);
+        snprintf(label_buffer, sizeof(label_buffer), "%d%%", (int)hum);
         lv_label_set_text(hum_val_label, label_buffer);
     }
 
@@ -204,12 +181,21 @@ int update_e_paper_display(void)
     float bat = get_latest(BATTERY_LEVEL);
     if (bat == -1)
     {
-        lv_label_set_text(battery_percentage_label, "BATT: n/a");
+        lv_label_set_text(batt_val_label, "n/a");
     }
     else
     {
-        snprintf(label_buffer, sizeof(label_buffer), "BATT: %d%%", (int)bat);
-        lv_label_set_text(battery_percentage_label, label_buffer);
+        snprintf(label_buffer, sizeof(label_buffer), "%d%%", (int)bat);
+        lv_label_set_text(batt_val_label, label_buffer);
+    }
+
+    refresh_count++;
+    if (refresh_count >= 10)
+    {
+        // Full refresh every 10th update to avoid ghosting
+        display_blanking_on(epd_dev);
+        display_blanking_off(epd_dev);
+        refresh_count = 0;
     }
 
     lv_task_handler();
