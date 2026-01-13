fceaa1cd2f520c138a39f2a79675b17e70d3c787
Paavo Palomäki <paavo.palomaki@gmail.com>
2026-01-13 23:16:08 +0200

    Enable warmup only when using SGP40 sensor

--- firmware/src/air_quality_monitor.c
+++ firmware/src/air_quality_monitor.c
@@ -117,9 +117,11 @@ static void periodic_task(struct k_work *work)
 
     // Set state to measuring and idle idle some time for sensor warmup
     set_state(MEASURING);
+#ifdef CONFIG_ENABLE_SGP40
     LOG_INF("Sensor warming up started, waiting for %d ms.", CONFIG_SENSOR_WARMUP_TIME_MS);
     k_sleep(K_MSEC(CONFIG_SENSOR_WARMUP_TIME_MS));
     LOG_INF("Sensor warm up complete.");
+#endif
 
 #ifdef CONFIG_ENABLE_BATTERY_MONITOR
     LOG_INF("Reading battery level.");
